244f25893acdd8452d69f6e33eb89d88a5798c68
Bradley Tenuta <bradleyt2009@hotmail.co.uk>
2017-11-20 20:18:16 +0000

    Started the work on the Dashboard. Also added a picture which will work as the banner image for the dashboard.

--- Artatawe.java
+++ Artatawe.java
@@ -4,13 +4,19 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderImage;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -92,8 +98,10 @@ public class Artatawe extends Application {
 				errorLabel.setStyle("-fx-text-fill: red");
 			}else if(true){
 				//add check for profile Exists, method return boolean
+				makeDashboard();
 			}else {
 				//go to dashboard.
+				makeDashboard();
 			}
 		});
 		
@@ -121,5 +129,72 @@ public class Artatawe extends Application {
 		loginVBox.requestFocus();
 		
 		return scene;
+	}
+
+	public void makeDashboard() {
+		BorderPane mainBorderPane = new BorderPane();
+		mainBorderPane.setId("mainBorderPane");
+		
+		//Creation of everything in the top BorderPane
+		VBox mainBorderPaneTopVBox = new VBox();
+		
+		StackPane bannerSP = new StackPane();
+		
+		Image bannerImage = new Image("bannerImage.png");
+		ImageView bannerImageView = new ImageView();
+		bannerImageView.setImage(bannerImage);
+		
+		
+		Label bannerTitle = new Label("Artatawe");
+		bannerTitle.setId("BannerTitle");
+		bannerSP.setMargin(bannerTitle,new Insets(0,800,0,0));
+		
+		bannerSP.getChildren().addAll(bannerImageView, bannerTitle);
+		
+		HBox navigationBar = new HBox();
+		navigationBar.setId("navigationBar");
+		Button viewAuctions = new Button("Auctions");
+		Button viewProfile = new Button("My Account");
+		viewAuctions.setId("viewAuctionsButton");
+		viewProfile.setId("viewProfileButton");
+		navigationBar.getChildren().addAll(viewAuctions, viewProfile);
+		
+		HBox auctionNavigationBar = new HBox();
+		auctionNavigationBar.setId("auctionNavigationBar");
+		auctionNavigationBar.setMaxHeight(40);
+		auctionNavigationBar.setMinHeight(40);
+		
+		Label filterlabel = new Label("Filter:");
+		CheckBox paintingCheckBox = new CheckBox("Painting");
+        CheckBox sculptureCheckBox = new CheckBox("Scultpure");
+
+		Label searchLabel = new Label("Search:");
+		TextField searchUserArtwork = new TextField();
+		searchUserArtwork.setPromptText("Username");
+		
+		filterlabel.setId("filterlabel");
+		searchLabel.setId("searchLabel");
+		searchUserArtwork.setId("searchUserArtwork");
+		paintingCheckBox.setId("filterPaintingCheckBox");
+		sculptureCheckBox.setId("filterSculptureCheckBox");
+
+		auctionNavigationBar.getChildren().addAll(filterlabel, paintingCheckBox, sculptureCheckBox, searchLabel, searchUserArtwork);
+		auctionNavigationBar.setMargin(searchUserArtwork,new Insets(7,0,0,0));
+		auctionNavigationBar.setMargin(paintingCheckBox,new Insets(10,10,0,0));
+		auctionNavigationBar.setMargin(sculptureCheckBox,new Insets(10,10,0,0));
+		
+		mainBorderPaneTopVBox.getChildren().addAll(bannerSP, navigationBar, auctionNavigationBar);
+		
+		//Creation of everything in the center BorderPane
+		
+		ScrollPane borderPaneSP = new ScrollPane();
+		
+		
+		//Sets the main BorderPane properties
+		mainBorderPane.setTop(mainBorderPaneTopVBox);
+		mainBorderPane.setCenter(borderPaneSP);
+		
+		artataweScene.setRoot(mainBorderPane);
+		mainBorderPane.requestFocus();
 	}
 }
