f8b59e58ca9dadcf8bc233757e84029a8aa83704
JJ-Markey <33732283+JJ-Markey@users.noreply.github.com>
2017-12-03 21:58:01 +0000

    Profile should be working fine, auction being worked on

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -1,17 +1,19 @@
 import java.util.*;
 import java.io.*;
 /**
- * 
+ * Loads and creates objects
  * @author Jay Markey
  *
  */
 public class FileLoader {
-	private final String profilePath = "C:\\Users\\Jay\\eclipse-workspace\\A3\\Profiles";
-	private final String auctionPath = "C:\\Users\\Jay\\eclipse-workspace\\A3\\Auctions";
+	private final String profilePath = "C:\\Users\\Jay\\eclipse-workspace\\A3Test\\Profiles";
+	private final String auctionPath = "C:\\Users\\Jay\\eclipse-workspace\\A3Test\\Auctions";
+	private ArrayList<Auction> auctions = new ArrayList<>();
+	private ArrayList<Profile> profiles = new ArrayList<>();
 	
 	
-	public void loadProfile(){
-		ProfileManager pm = new ProfileManager();
+	public ArrayList<Profile> loadProfile(){
+		
 		try {
 			File file = new File(profilePath);
 			
@@ -32,7 +34,7 @@ public class FileLoader {
 				
 				Profile p = new Profile(username, firstName, lastName, 
 						                telephone, address, postcode, imagePath);
-				pm.addProfile(p);
+				profiles.add(p);
 				
 			}
 			in.close();
@@ -40,20 +42,119 @@ public class FileLoader {
 			System.out.println(profilePath + "not found");
 			System.exit(0);
 		}
-		
+		return this.profiles;
 	}
 	
-	public void loadAuction(){
+	public void loadPainting(Scanner in) {
+		Profile bidder = null;
+		
+		String curLine = in.nextLine();
+		Scanner line = new Scanner(curLine);
+		line.useDelimiter(",");
+		
+		String title = line.next();
+		String description = line.next();
+		String photo = line.next();
+		String creatorsName = line.next();
+		int creationYear = line.nextInt();
+		double price = line.nextDouble();
+		int bidTotal = line.nextInt();
+		String dateTime = line.next();
+		int width = line.nextInt();
+		int height = line.nextInt();
+			
+		boolean complete = line.nextBoolean();
+			
+		Painting pnt = new Painting(title, description, photo, creatorsName, creationYear, 
+				price, bidTotal, dateTime, width, height);
+			
+		Auction au = new Auction(pnt, complete);
+		auctions.add(au);
+			
+		while (line.next() != null) {
+			double amount = line.nextDouble();
+			String dateBid = line.next();
+			String usernameBid = line.next();
+				
+			for (Profile p : this.profiles) {
+				if (p.getUsername().equals(usernameBid)) {
+					bidder = p;
+				}	
+			}
+				
+			Bid b = new Bid(bidder, pnt, amount, dateBid);
+			au.placeBid(b);
+			}
+			line.close();
+	}
+	
+	public void loadSculpture(Scanner in) {
+		Profile bidder = null;
+		
+		String curLine = in.nextLine();
+		Scanner line = new Scanner(curLine);
+		line.useDelimiter(",");
+			
+		String title = line.next();
+		String description = line.next();
+		String photo = line.next();
+		String creatorsName = line.next();
+		int creationYear = line.nextInt();
+		double price = line.nextDouble();
+		int bidTotal = line.nextInt();
+		String dateTime = line.next();
+		int width = line.nextInt();
+		int height = line.nextInt();
+		int depth = line.nextInt();
+		String material = line.next();
+			
+		Sculpture scu = new Sculpture(title, description, photo, creatorsName, creationYear, price, bidTotal,
+				dateTime, width, height, depth, material);
+			
+		while (line.next() != "£") {
+			scu.getExtraPhotos().add(line.next());
+		}
+			
+			
+		boolean complete = line.nextBoolean();
+			
+		Auction au = new Auction(scu, complete);
+		auctions.add(au);
+			
+		while (line.next() != null) {
+			double amount = line.nextInt();
+			String dateBid = line.next();
+			String usernameBid = line.next();
+				
+			for (Profile p : this.profiles) {
+				if (p.getUsername().equals(usernameBid)) {
+					bidder = p;
+				}	
+			}
+				
+			Bid b = new Bid(bidder, scu, amount, dateBid);
+			au.placeBid(b);
+		}
+	line.close();
+}
+	
+	
+	public ArrayList<Auction> loadAuction(){
 		try { 
 			File file = new File(auctionPath);
 			
 			Scanner in = new Scanner(file);
 			
 			while (in.hasNextLine()) {
-				String title = in.nextLine();
-				String description = in.nextLine();
-				String image = in.nextLine();
-				String creator = in.nextLine();
+				if (in.next().contains("Painting")) {
+					this.loadPainting(in);
+					System.out.println("yo yo");
+				} else if (in.next().contains("Sculpture")) {
+					this.loadSculpture(in);
+					System.out.println("yo im here");
+				}else {
+					break;
+				}
 				
 			}
 			
@@ -63,5 +164,6 @@ public class FileLoader {
 			System.exit(0);
 		}
 		
+		return this.auctions;
 	}
 }
