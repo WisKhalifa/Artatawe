0f1187c9747eaf7c08e705e4f1c51c4b890f942f
JJ-Markey <33732283+JJ-Markey@users.noreply.github.com>
2017-12-04 23:50:29 +0000

    have 1 bug to fix and then commenting

--- src/FileLoader.java
+++ src/FileLoader.java
@@ -6,14 +6,13 @@ import java.io.*;
  *
  */
 public class FileLoader {
-	private final String profilePath = "profile.txt";
-	private final String auctionPath = "auction.txt";
+	private final String profilePath = "C:\\Users\\Jay\\eclipse-workspace\\A3Test\\Profiles";
+	private final String auctionPath = "C:\\Users\\Jay\\eclipse-workspace\\A3Test\\Auctions";
 	private ArrayList<Auction> auctions = new ArrayList<>();
 	private ArrayList<Profile> profiles = new ArrayList<>();
 	
 	
 	public ArrayList<Profile> loadProfile(){
-		
 		try {
 			File file = new File(profilePath);
 			
@@ -28,30 +27,29 @@ public class FileLoader {
 				String postcode = in.nextLine();
 				String imagePath = in.nextLine();
 				
-				//used to skip over the line with , 
-				if (in.nextLine() == ",") {
-					continue; 
+				Profile p = new Profile(username, firstName, lastName, 
+		                telephone, address, postcode, imagePath);
+				
+				String nextElem = in.nextLine();
+				
+				while(!(nextElem.equals(","))) {
+					p.addFavourite(nextElem);
+					System.out.println(nextElem);
+					nextElem = in.nextLine();
 				}
 				
-				Profile p = new Profile(username, firstName, lastName, 
-						                telephone, address, postcode, imagePath);
 				profiles.add(p);
-				
 			}
 			in.close();
 		} 	catch (FileNotFoundException e) {
 			System.out.println(profilePath + "not found");
 			System.exit(0);
 		}
-		return this.profiles;
+		return profiles;
 	}
 	
-	public void loadPainting(Scanner in) {
+	public void loadPainting(Scanner line) {
 		Profile bidder = null;
-		
-		String curLine = in.nextLine();
-		Scanner line = new Scanner(curLine);
-		line.useDelimiter(",");
 		
 		String title = line.next();
 		String description = line.next();
@@ -70,31 +68,29 @@ public class FileLoader {
 				price, bidTotal, dateTime, width, height);
 			
 		Auction au = new Auction(pnt, complete);
-		auctions.add(au);
-			
-		while (line.next() != null) {
-			double amount = line.nextDouble();
-			String dateBid = line.next();
-			String usernameBid = line.next();
-				
-			for (Profile p : this.profiles) {
-				if (p.getUsername().equals(usernameBid)) {
-					bidder = p;
-				}	
-			}
-				
-			Bid b = new Bid(bidder, pnt, amount, dateBid);
-			au.placeBid(b);
-			}
-			line.close();
-	}
-	
-	public void loadSculpture(Scanner in) {
-		Profile bidder = null;
 		
-		String curLine = in.nextLine();
-		Scanner line = new Scanner(curLine);
-		line.useDelimiter(",");
+		while (line.hasNext()) {
+			if (line.next().equals(".")) {
+				double amount = line.nextDouble();
+				String dateBid = line.next();
+				String usernameBid = line.next();
+							
+				for (Profile p : this.profiles) {
+					if (p.getUsername().equals(usernameBid)) {
+						bidder = p;
+					}	
+				}
+							
+				Bid b = new Bid(bidder, pnt, amount, dateBid);
+				au.placeBid(b);
+			}	
+		}
+		auctions.add(au);
+	}	
+	
+	public void loadSculpture(Scanner line) {
+		ArrayList<String> extraPhotos = new ArrayList<>();
+		Profile bidder = null;
 			
 		String title = line.next();
 		String description = line.next();
@@ -111,32 +107,38 @@ public class FileLoader {
 			
 		Sculpture scu = new Sculpture(title, description, photo, creatorsName, creationYear, price, bidTotal,
 				dateTime, width, height, depth, material);
-			
-		while (line.next() != "£") {
-			scu.getExtraPhotos().add(line.next());
+		
+		String nextPhoto = line.next();
+		
+		while(!(nextPhoto.equals("!"))) {
+			extraPhotos.add(nextPhoto);
+			System.out.println(nextPhoto);
+			nextPhoto = line.next();
 		}
-			
+		
+		scu.setExtraPhotos(extraPhotos);
 			
 		boolean complete = line.nextBoolean();
 			
 		Auction au = new Auction(scu, complete);
-		auctions.add(au);
-			
-		while (line.next() != null) {
-			double amount = line.nextInt();
-			String dateBid = line.next();
-			String usernameBid = line.next();
-				
-			for (Profile p : this.profiles) {
-				if (p.getUsername().equals(usernameBid)) {
-					bidder = p;
-				}	
-			}
-				
-			Bid b = new Bid(bidder, scu, amount, dateBid);
-			au.placeBid(b);
+		
+		while (line.hasNext()) {
+			if (line.next().equals(".")) {
+				double amount = line.nextDouble();
+				String dateBid = line.next();
+				String usernameBid = line.next();
+							
+				for (Profile p : this.profiles) {
+					if (p.getUsername().equals(usernameBid)) {
+						bidder = p;
+					}	
+				}
+							
+				Bid b = new Bid(bidder, scu, amount, dateBid);
+				au.placeBid(b);
+			}	
 		}
-	line.close();
+		auctions.add(au);
 	}
 	
 	
@@ -146,25 +148,27 @@ public class FileLoader {
 			
 			Scanner in = new Scanner(file);
 			
-			while (in.hasNextLine()) {
-				if (in.next().contains("Painting")) {
-					this.loadPainting(in);
-					System.out.println("yo yo");
-				} else if (in.next().contains("Sculpture")) {
-					this.loadSculpture(in);
-					System.out.println("yo im here");
-				}else {
-					break;
-				}
-				
-			}
 			
+			while (in.hasNextLine()) {
+				String curLine = in.nextLine();
+				Scanner line = new Scanner(curLine);
+				line.useDelimiter(",");
+				
+				if (line.next().equalsIgnoreCase("Painting")) {
+					loadPainting(line);
+					System.out.println("yo yo");
+					
+				} else if (line.next().equalsIgnoreCase("Sculpture")) { 
+					loadSculpture(line);
+					System.out.println("yo im here");
+				}
+				line.close();
+			}
 			in.close();
 		} catch (FileNotFoundException e) {
 			System.out.println(auctionPath + "not found");
 			System.exit(0);
 		}
-		
-		return this.auctions;
+		return auctions;
 	}
 }
