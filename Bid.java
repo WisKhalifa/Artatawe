5e096af652d5b63e4c940782fa0a8da4be23956c
cormaclee <33754788+cormaclee@users.noreply.github.com>
2017-12-10 15:49:23 +0000

    Added javadoc comments to all of my classes

--- Bid.java
+++ Bid.java
@@ -1,21 +1,21 @@
-
-
 /**
- *
- * @author Cormac Anderson
+ * This class simulates a bid for an auction.
+ * @author Cormac Anderson 911013
  */
 public class Bid {
+    
+        // Instance values
         private Profile bidder;
         private Artwork artwork;
         private double amount;
         private String date;
         
         /**
-         * 
-         * @param p
-         * @param a
-         * @param amount
-         * @param date 
+         * Constructs an instance of a bid.
+         * @param p The profile of the user placing the bid.
+         * @param a The artwork the bid is being placed upon.
+         * @param amount The amount of the bid.
+         * @param date The date the bid was placed.
          */
         public Bid(Profile p, Artwork a, double amount, String date) {
                 this.bidder = p;
@@ -25,32 +25,28 @@ public class Bid {
         }
         
         /**
-         * 
-         * @return 
+         * @return The profile of the user who placed the bid. 
          */
         public Profile getBidder() {
                 return bidder;
         }
         
         /**
-         * 
-         * @return 
+         * @return The artwork the bid is being placed upon.
          */
         public Artwork getArtwork() {
                 return artwork;
         }
         
         /**
-         * 
-         * @return 
+         * @return The amount of the bid.
          */
         public double getAmount() {
                 return amount;
         }
         
         /**
-         * 
-         * @return 
+         * @return The date the bid was placed.
          */
         public String getDate() {
                 return date;
