e87f6ff402b0b699d4bac4b4018a1b95971def52
Liam Cooper <liamcooper@liams-air.home>
2017-12-10 03:30:25 +0000

    fully commented artwork, painting and sculpture

--- Artwork.java
+++ Artwork.java
@@ -1,9 +1,8 @@
 /**
+ * this class provides the basic data for all artwork's 
  * @author Liam Cooper - 916153
+ * @version 1.2
  */
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public abstract class Artwork {
 
@@ -15,7 +14,17 @@ public abstract class Artwork {
   private double price;
   private int bidTotal;
   private String dateTime;
-
+  
+  /**
+   * constructs artwork
+   * @param title
+   * @param description
+   * @param photo
+   * @param creatorsName
+   * @param creationYear
+   * @param price
+   * @param dateTime
+   */
   public Artwork(String title, String description, String photo, String creatorsName,
     int creationYear, double price, String dateTime) {
 
@@ -29,74 +38,133 @@ public abstract class Artwork {
       this.dateTime = dateTime;
 
   }
-
+  
+  /**
+   * gets the date and time of when auction was placed
+   * @return dateTime
+   */
   public String getDateTime() {
     return dateTime;
   }
-
+  
+  /**
+   * gets the title of the artwork
+   * @return title
+   */
   public String getTitle() {
     return title;
   }
-
+  
+  /**
+   * gets the description of the artwork
+   * @return description
+   */
   public String getDescription() {
     return description;
   }
 
+  /**
+   * gets the main photo of the artwork
+   * @return photo
+   */
   public String getMainPhoto() {
     return photo;
   }
 
+  /**
+   * gets the name of the artworks creator
+   * @return creatorsName
+   */
   public String getCreatorName() {
     return creatorsName;
   }
 
+  /**
+   * gets the year the artwork was created
+   * @return creationYear
+   */
   public int getCreationYear() {
     return creationYear;
   }
 
+  /**
+   * gets the price of the artwork
+   * @return price
+   */
   public double getPrice() {
     return price;
   }
 
+  /**
+   * gets the maximum amount of bids for the artwork 
+   * @return bidTotal 
+   */
   public int getBidTotal() {
     return bidTotal;
   }
 
+  /**
+   * sets the date and time of when the artwork was placed 
+   * @param dateTime
+   */
   public void setDateTime(String dateTime) {
     this.dateTime = dateTime;
   }
 
+  /**
+   * sets the title of the artwork 
+   * @param title
+   */
   public void setTitle(String title) {
     this.title = title;
   }
 
+  /**
+   * sets the description of the artwork 
+   * @param description
+   */
   public void setDescription(String description) {
     this.description = description;
   }
 
+  /**
+   * sets the photo of the artwork
+   * @param photo
+   */
   public void setMainPhoto(String photo) {
     this.photo = photo;
   }
 
+  /**
+   * sets the name of the user that created the artwork 
+   * @param creatorsName
+   */
   public void setCreatorName(String creatorsName) {
     this.creatorsName = creatorsName;
   }
 
+  /**
+   * sets the year of which the artwork was created 
+   * @param creationYear
+   */
   public void setCreationYear(int creationYear) {
     this.creationYear = creationYear;
   }
 
+  /**
+   * sets the price of the artwork 
+   * @param price
+   */
   public void setPrice(double price) {
     this.price = price;
   }
 
+  /**
+   * sets the maximum amount of bids allowed on the artwork
+   * @param bidTotal
+   */
   public void setBidTotal (int bidTotal) {
     this.bidTotal = bidTotal;
   }
 
-  /*public String toString() {
-    return "\nTitle: " + getTitle() + "\nDescription: " + getDescription() + "\nPhoto: " + getMainPhoto() +
-      "\nCreators Name: " + getCreatorName() + "\nYear: " + getCreationYear() + "\nPrice: £" + getPrice() +
-        "\nbid total: " + getBidTotal();
-  }*/
 }
--- Sculpture.java
+++ Sculpture.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
-
 /**
+ * this class inherits data from artwork and creates a sculpture
  * @author Liam Cooper - 916153
+ * @version 1.2
  */
-
+import java.util.ArrayList;
 public class Sculpture extends Artwork {
 
   private int width;
@@ -11,7 +11,21 @@ public class Sculpture extends Artwork {
   private int depth;
   private String material;
   private ArrayList<String> extraPhotos;
-
+  
+  /**
+   * Constructs a Sculpture 
+   * @param title
+   * @param description
+   * @param photo
+   * @param creatorsName
+   * @param creationYear
+   * @param price
+   * @param dateTime
+   * @param width
+   * @param height
+   * @param depth
+   * @param material
+   */
   public Sculpture(String title, String description, String photo, String creatorsName,
     int creationYear, double price, String dateTime, int width, int height,
       int depth, String material) {
@@ -24,52 +38,84 @@ public class Sculpture extends Artwork {
     extraPhotos = new ArrayList<>();
   }
 
+  /**
+   * gets the width of the sculpture
+   * @return width
+   */
   public int getWidth() {
     return width;
   }
 
+  /**
+   * gets the height of the sculpture
+   * @return height
+   */
   public int getHeight() {
     return height;
   }
 
+  /**
+   * gets the depth of the sculpture
+   * @return depth
+   */
   public int getDepth() {
     return depth;
   }
-
+  
+  /**
+   * gets the material the sculpture is made of
+   * @return material
+   */
   public String getMaterial() {
     return material;
   }
 
+  /**
+   * gets the extra photos of the sculpture
+   * @return extraPhotos
+   */
   public ArrayList<String> getExtraPhotos() {
     return extraPhotos;
   }
 
+  /**
+   * sets the width of the sculpture
+   * @param width
+   */
   public void setWidth (int width) {
     this.width = width;
   }
-
+  
+  /**
+   * sets the height of the sculpture
+   * @param height
+   */
   public void setHeight (int height) {
     this.height = height;
   }
-
+  
+  /**
+   * sets the depth of the sculpture
+   * @param depth
+   */
   public void setDepth (int depth) {
     this.depth = depth;
   }
 
+  /**
+   * sets the material ths sculpture is made of
+   * @param material
+   */
   public void setMaterial (String material) {
     this.material = material;
   }
 
+  /**
+   * sets the extra photos of the sculpture
+   * @param extraPhotos
+   */
   public void setExtraPhotos (ArrayList<String> extraPhotos) {
     this.extraPhotos = extraPhotos;
   }
-
-  /*public String toString() {
-    return "\nSculpture -" + "\nTitle: " + getTitle() + "\nDescription: " + getDescription() + "\nPhoto: " + getMainPhoto() +
-      "\nCreators Name: " + getCreatorName() + "\nYear: " + getCreationYear() + "\nPrice: £" + getPrice() +
-        "\nbid total: " + getBidTotal() + "\nWidth: " + getWidth() +
-          "\nHeight: " + getHeight() + "\nDepth: " + getDepth() + "\nMaterial: " + getMaterial() + "\nPhotos: " +
-            getExtraPhotos();
-  }*/
 
 }
